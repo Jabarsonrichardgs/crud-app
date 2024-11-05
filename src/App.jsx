18d283ff13149f6482a64d32f06f0dcdde2a3208
Jabarsonrichardgs <jabarsonrichardofficial@gmail.com>
2024-11-05 21:52:18 +0530

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import List from "./components/list";
+
 import "./App.css";
 import Create from "./components/create";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Edit from "./components/Edit";
 import ViewDetails from "./components/ViewDetails";
+import List from "./components/list";
 
 const App = () => {
   return (
